f3c5f9da92d8118cff1310c8b23426d8c7380ec0
Saleh Souzanchi <saleh.souzanchi@gmail.com>
2021-11-30 01:02:15 +0330

    add GetInvoiceList Method

--- src/Invoice.php
+++ src/Invoice.php
@@ -10,16 +10,23 @@ namespace HolooClient;
 class Invoice extends Holoo
 {
 
+    
     /**
-     * GetCusItomer
-     *
+     * GetInvoiceList
+     * fetch all or one more invoides
+     * 
+     * ### Example
+     * ```
+     * $invioces= Invoice::GetInvoiceList();
+     * $invioces= Invoice::GetInvoiceList(['Code'=>123]);
+     * $invioces= Invoice::GetInvoiceList(['type'=>4]);
+     * ```
+     * 
      * @param  mixed $params
-     * @return void
+     * @return array
      */
-    public static function GetInvoiceList($params = [])
+    public static function GetInvoiceList($params = []): array
     {
-        return self::getRequest('Invoice/Invoice',$params);
+        return self::getRequest('Invoice/Invoice', $params);
     }
-
-
 }
